1af0bcfbb2bbba180d43592742f54f671ed8a061
Carmen Lucero Sandoval <carmenlucero786@gmail.com>
2019-03-14 13:11:06 -0500

    updated files

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,9 +2,10 @@
 // ==============================  Challenge #1: Customer View   ====================================
 
 
+require("dotenv").config();
+console.log(process.env.MY_PASSWORD);  //process global object 
 /* ===================================  TEST CODE:  =============================================================
 //add code to read and set any environment variables with the dotenv package:
-require("dotenv").config();
 // add the code required to import my password.js file and store it in a variable.
 var importedPWD = require("./password.js");  //my password was made available thanks to MODULARIZATION
 // now, my password can be accessed with the following line:
@@ -20,18 +21,15 @@ process.env was created by adding ENVIRONMENT SPECIFIC VARIABLES to my .env file
 var fs = require("fs");
 
 function randomCommand() {
-        fs.readFile(".env", "utf8", function(err, data) {
+        fs.readFile(".env", "utf8", function(err, data) { //not this file
           if(err) {
             return console.log(err);
           }
-          var data = data.split("=");
-          //var commandArg = data[0];
-          var songArg = data[1];
-          console.log(songArg);
+          console.log(data.split("=")[1]);
         })
 };
 randomCommand(); 
- =============================================================================================================== */
+=============================================================================================================== */
 
 // require modules
 var inquirer = require('inquirer');
@@ -59,7 +57,7 @@ var connection = mysql.createConnection({
     host: 'localhost',      //mysql server is connected only to my localhost
     port: 3306,             //this is mysql port that the module comes with
     user: 'root',           //user should be 'root' unless I changed it to something else(no creo)
-    password: 'Exito2019*',
+    password: process.env.MY_PASSWORD,
     database: 'bamazon_DB' //this line indicates to which database we are connecting to (must have already been created)  
 });
             
@@ -79,17 +77,24 @@ connection.connect(function(err) {
     // ============== start();
 });
                    
-
+// this should be a CRUD APP 
+// I have already 'created' a table
+// showProducts() is reading and displaying my table data
 function showProducts() {
     // query the database for all  products available for sale. Here we are connecting to mysql table
     connection.query('SELECT * FROM products', function(err, results) {
         if(err) throw err;
         // once we have connected to MySQL, prompt user with available products for sale
-        console.log(results);
+        //console.log(results);
+        for(var i = 0; i < results.length; i++){
+            console.log("ITEM ID: " + results[i].id  + "PRODUCT NAME: " + results[i].product_name + "DEPARTMENT: " + results[i].department_name  + "PRICE: " + results[i].price +  "QUANTITY: " + results[i].stock_quantity);
+          }
     });
 }
 showProducts();
 
+
+// again, productID() is reading our table data (id) if question type: list
 // 6. The app should then prompt users with two messages.
 function productID() {
     //query the database for all products on sale. 
@@ -102,15 +107,22 @@ function productID() {
                 // The first should ask them the ID of the product they would like to buy.
                 name: 'chooseID',
                 type: 'input',
-                //type: 'rawlist',
+                //type: 'list',
                 message: 'To place your order, select the product\'s id:',
+                filter: Number,   
+                validate: function(value) {
+                    if (isNaN(value) === false) {
+                      return true;
+                    }
+                    return false;
+                  }
                 /*
                 choices: function() {
                     var choices = [];
                     //loop through the results
                     for(var i = 0; i < results.length; i++) {
-                        //push results[i].item_id into choices[]
-                        choices.push(results[i].stock_quantity);
+                        //push results[i].id into choices[]
+                        choices.push(results[i].id);
                     }
                     return choices;
                 } //choices function(){} ends
@@ -121,18 +133,35 @@ function productID() {
                 name: 'numUnits',
                 type: 'list',
                 message: 'How many usnits would you like to buy?',
-                choices: ['1','2','3','4','5']
+                choices: ['1','2','3','4','5', '11'],
             }
         ]) //prompt({}) ends
         .then(function(answers) {
-            console.log(answers);
+            console.log(answers);  //works as expected
+            console.log(answers.chooseID);  //test: works as expected
+            //var chosenItem = results[i].id;
+            //var chosenItem; 
+            for(var i = 0; i < results.length; i++) {
+                if(results[i].id === answers.chooseID && results[i].stock_quantity >= answers.numUnits) {  //works as expected
+                    console.log('yes');
+                }
+                else {
+                    console.log('no');
+                }
+            } //for loop closes
+
         }) //.then(){} ends
     }); //connection.query({}) ends
 };  //productID() ends
 productID();
 
+/* 7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through. */
 
 
+/* 8. However, if your store does have enough of the product, you should fulfill the customer's order.
+This means updating the SQL database to reflect the remaining quantity.
+Once the update goes through, show the customer the total cost of their purchase.  */
 
 
 
